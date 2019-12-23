555d7d6d04c2c80864a85a6cfcb87951b5d43e7b
MarcioQuimbundo <marcioquimbundo@hotmail.com>
2019-12-23 17:43:48 +0100

    test added

--- lib/wesender_flutter.dart
+++ lib/wesender_flutter.dart
@@ -1,7 +1,37 @@
 library wesender_flutter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:http/http.dart';
+
+Map<String, String> requestHeaders = {
+  'Content-Type': 'application/json',
+};
+Client client;
+
+class WeSenderSDK {
+  static const String _url = "https://api.wesender.co.ao";
+  String _apiKey;
+
+  WeSenderSDK({apiKey}) {
+    _apiKey = apiKey;
+  }
+
+  Future sendMessage({destine, message, hasSpecialCharacters = false}) async {
+    const route = "envio/apikey";
+    const fullPath = _url + route;
+
+    Map<String, dynamic> body = {
+      "ApiKey": _apiKey,
+      "Mensagem": message,
+      "Destino": destine,
+      "CEspecial": hasSpecialCharacters
+    };
+
+    try {
+      var sendStreambleResponse =
+          await client.post(fullPath, headers: requestHeaders, body: body);
+      return sendStreambleResponse;
+    } catch (e) {
+      throw e;
+    }
+  }
 }
--- test/wesender_flutter_test.dart
+++ test/wesender_flutter_test.dart
@@ -3,11 +3,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:wesender_flutter/wesender_flutter.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  test('user can send message from apikey', () async {
+    final weSender = WeSenderSDK(apiKey: "");
+    Map<String, dynamic> expected = {"Exito": true};
+
+    var response = await weSender.sendMessage(
+        destine: ["000000000"],
+        message: "Hello World",
+        hasSpecialCharacters: true);
+    expect(response, expected);
   });
 }
